f59e5fcce3b39764d43ba188bf9a550d21e180ed
jufa2401 <jufa2401@gmail.com>
2018-03-12 15:17:31 +0100

    fixed small things, added a highscore component, which may be implemented if time is available

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {WordComponent} from './word/word.component';
-import {StatusComponent} from './status/status.component';
 import {LoginComponent} from './login/login.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from '../app-routing.module';
 import {AuthguardService} from '../authguard.service';
 import {AuthService} from '../auth.service';
+import {HighscoreComponent} from './highscore/highscore.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
-    StatusComponent,
-    LoginComponent
+    LoginComponent,
+    HighscoreComponent
   ],
   imports: [
     BrowserModule,
--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -12,7 +12,8 @@ export class WordComponent implements OnInit {
   buttonLetters: string[];
   hiddenWord: string;
   symbol = '_';
-  images: string[] = ['./assets/GRAFIK/galge.png', './assets/GRAFIK/forkert1.png', './assets/GRAFIK/forkert2.png', './assets/GRAFIK/forkert3.png', './assets/GRAFIK/forkert4.png'
+  images: string[] = ['./assets/GRAFIK/galge.png', './assets/GRAFIK/forkert1.png',
+    './assets/GRAFIK/forkert2.png', './assets/GRAFIK/forkert3.png', './assets/GRAFIK/forkert4.png'
     , './assets/GRAFIK/forkert5.png', './assets/GRAFIK/forkert6.png'];
   letters: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
@@ -23,26 +24,26 @@ export class WordComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.buttonLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+    this.buttonLetters = this.letters;
     this.word = 'default';
     this.gameStatus = 'Welcome to hangman press a button to begin!';
     this.hiddenWord = this.word;
+
   }
 
   onLetterClick(letter: string) {
     if (this.imageIndex >= 6) {
       this.gameStatus = 'Du har tabt ordet var: ' + this.word + ', et nyt spil vil starte';
-      console.log(this.imageIndex);
       this.imageIndex = -1;
     }
-    console.log(letter);
-      const clickedLetter = this.word.match(letter);
+      const correctLetter = this.word.match(letter);
 
-      if (clickedLetter === null) {
+      if (correctLetter === null) {
+        console.log('Wrong letter ' + letter + ' was pressed');
         this.imageIndex++;
-        console.log(clickedLetter);
         return true;
+      } else {
+        console.log(correctLetter);
       }
 
       return false;

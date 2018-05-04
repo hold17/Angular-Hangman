0b784c917d2bc84dab3f6c37e9d0e3d02b8f3393
jufa2401 <jufa2401@gmail.com>
2018-05-04 12:22:50 +0200

    added name display when logged in. Going to add more html text elements for displaying the word and hints

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../auth.service';
 import {Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: []
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
   constructor(public auth: AuthService, private router: Router, private toastr: ToastrService) {}
+  name: string;
+  // laver en observable til at tjekke den token vi bruger
+  observeToken = new Observable((observer) => {
+    observer.next(localStorage.getItem('token'));
+    observer.complete();
+  });
   logout() {
     this.auth.logoutRemoveToken();
     this.toastr.success('You have succesfully logged out');
@@ -18,5 +25,13 @@ export class HeaderComponent  {
 
     goHome() {
       this.router.navigate(['/game']);
+  }
+
+  ngOnInit(): void {
+    // sætter name property til at være JSON objectets first name
+    this.observeToken.subscribe((res: string) => {
+      const tokenObj = JSON.parse(res);
+      this.name = tokenObj.user.firstname;
+    });
   }
 }
